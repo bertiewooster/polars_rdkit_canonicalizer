8fcc7a104a570ca342d58610cecc97678eda40f6
Jeremy Monat <jemonat@calalum.org>
2024-03-08 22:31:11 -0500

    Add rdkit crate, fn canonicalize

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import polars as pl
 
-from rdkit_canonicalizer import pig_latinnify
+from rdkit_canonicalizer import canonicalize, pig_latinnify
 
 df = pl.DataFrame({
     'english': ['this', 'is', 'not', 'pig', 'latin'],
@@ -8,3 +8,8 @@ df = pl.DataFrame({
 result = df.with_columns(pig_latin = pig_latinnify('english'))
 print(result)
 
+df = pl.DataFrame({
+    'sml': ["C1=CC=CC=C1", "C1C=CC=CC=1"],
+})
+result = df.with_columns(can_sml = canonicalize('sml'))
+print(result)
