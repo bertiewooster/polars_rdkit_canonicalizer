5bf885572ff0b568c4ac2381abec74883adebcea
Jeremy Monat <jemonat@calalum.org>
2024-05-29 23:35:02 -0400

    Uncomment out Python code that does the RDKit operations Update expected output

--- run.py
+++ run.py
@@ -1,20 +1,21 @@
 import polars as pl
 
-from rdkit_canonicalizer import pig_latinnify
+from rdkit_canonicalizer import canonicalize
 
-# from rdkit_canonicalizer import canonicalize
+# from rdkit_canonicalizer import pig_latinnify
 
-df_pig_latin = pl.DataFrame({
-    'english': ['this', 'is', 'not', 'pig', 'latin'],
-})
-result = df_pig_latin.with_columns(pig_latin = pig_latinnify('english'))
-print(result)
 
-# df = pl.DataFrame({
-#     'sml': ["C1=CC=CC=C1", "C1C=CC=CC=1"],
+# df_pig_latin = pl.DataFrame({
+#     'english': ['this', 'is', 'not', 'pig', 'latin'],
 # })
-# result = df.with_columns(can_sml = canonicalize('sml'))
+# result = df_pig_latin.with_columns(pig_latin = pig_latinnify('english'))
 # print(result)
 
+df = pl.DataFrame({
+    'sml': ["C1=CC=CC=C1", "C1C=CC=CC=1"],
+})
+result = df.with_columns(can_sml = canonicalize('sml'))
+print(result)
+
 # Expected result:
-# 'can_sml' = ['C1=CC=CC=C1', 'C1=CC=CC=C1']
+# ['c1ccccc1', 'c1ccccc1']
